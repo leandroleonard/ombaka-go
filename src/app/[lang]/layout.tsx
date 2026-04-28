b35f831d59e4a376c916133d03ec29769a1c04db
Leandro Ventura <leandroventur19@gmail.com>
2026-04-28 13:11:03 +0100

    alteracao de fonts e estilos globais do site

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,11 +1,43 @@
-import type { Metadata } from 'next';
+// app/[lang]/layout.tsx
+
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { routing } from '@/i18n/routing';
-import '../globals.css';
+import { Montserrat, Poppins, Open_Sans, Lato } from 'next/font/google';
+import "./../globals.css";
 
 import { Header } from '@/app/components/Header';
 import { Footer } from '@/app/components/Footer';
+
+// Configurar as fontes (fora do componente)
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-montserrat',
+  display: 'swap',
+});
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['600'],
+  variable: '--font-poppins',
+  display: 'swap',
+});
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-open-sans',
+  display: 'swap',
+});
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['300'],
+  style: ['italic'],
+  variable: '--font-lato',
+  display: 'swap',
+});
 
 export default async function RootLayout({
   children,
@@ -21,7 +53,7 @@ export default async function RootLayout({
   const messages = await getMessages({ locale });
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className={`${montserrat.variable} ${poppins.variable} ${openSans.variable} ${lato.variable}`}>
       <body>
         <NextIntlClientProvider messages={messages}>
           <Header />
